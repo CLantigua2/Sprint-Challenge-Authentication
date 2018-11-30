9c8fe0add47351a78f3a0d62aa228a649618a1a1
Carlos Lantigua <clantigua954@gmail.com>
2018-11-30 11:38:47 -0500

    completed autherization and authentication

--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,53 @@
 const axios = require('axios');
+const db = require('../database/dbConfig.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const { authenticate } = require('./middlewares');
+const { authenticate, generateToken } = require('./middlewares');
 
-module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+module.exports = (server) => {
+	server.post('/api/register', register);
+	server.post('/api/login', login);
+	server.get('/api/jokes', authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+	// implement user registration
+	const creds = req.body;
+	const hash = bcrypt.hashSync(creds.password, 10);
+	creds.password = hash;
+	db('users')
+		.insert(creds)
+		.then((ids) => {
+			res.status(201).json(ids);
+		})
+		.catch((err) => res.status(204).json(err));
 }
 
 function login(req, res) {
-  // implement user login
+	// implement user login
+	const creds = req.body;
+	db('users')
+		.where({ username: creds.username })
+		.first()
+		.then((user) => {
+			if (user && bcrypt.compareSync(creds.password, user.password)) {
+				const token = generateToken(user);
+				res.status(200).json({ message: 'Welcome!', token });
+			} else {
+				res.status(401).json({ message: 'you shall not pass!!' });
+			}
+		})
+		.catch((err) => res.status(400).json(err));
 }
 
 function getJokes(req, res) {
-  axios
-    .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
-    )
-    .then(response => {
-      res.status(200).json(response.data);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
-    });
+	axios
+		.get('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten')
+		.then((response) => {
+			res.status(200).json(response.data);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+		});
 }
